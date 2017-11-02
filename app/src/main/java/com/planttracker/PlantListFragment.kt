da29a664bbba31cdc7094d91bbfb4fb53f4729ff
Cece <ceceliastory@gmail.com>
2017-11-02 14:09:42 -0500

    Displays all plants in a list

--- app/src/main/java/com/planttracker/PlantListFragment.kt
+++ app/src/main/java/com/planttracker/PlantListFragment.kt
@@ -1,10 +1,13 @@
 package com.planttracker
 
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ListView
 import android.widget.TextView
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
@@ -15,15 +18,21 @@ class PlantListFragment : Fragment() {
 
     @Inject lateinit var plantViewModel: PlantViewModel
     private val subscriptions = CompositeDisposable()
+    private lateinit var plantAdapter: PlantListAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         App.appComponent.inject(this)
+        plantAdapter = PlantListAdapter(context)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.plant_list_fragment, container, false)
+        val view = inflater.inflate(R.layout.plant_list_view, container, false)
+
+        val listView = view.findViewById<ListView>(R.id.list_plants) as ListView
+        listView.adapter = plantAdapter
+
+        return view
     }
 
     override fun onStart() {
@@ -43,7 +52,40 @@ class PlantListFragment : Fragment() {
                         .subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
                         .subscribe(
-                                { result -> view?.findViewById<TextView>(R.id.plant_name)?.text = result[0].name }
+                                { result ->
+                                    run {
+                                        plantAdapter.plants = result
+                                        plantAdapter.notifyDataSetChanged()
+                                    }
+                                }
                         ))
+    }
+
+    class PlantListAdapter(context: Context) : BaseAdapter() {
+
+        var plants: List<Plant> = emptyList()
+        private val layoutInflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val rowView = layoutInflater.inflate(R.layout.plan_list_detail_view, parent, false)
+            val plant: Plant = getItem(position) as Plant
+            val titleTextView = rowView.findViewById<TextView>(R.id.plant_name) as TextView
+
+            titleTextView.text = plant.name
+
+            return rowView
+        }
+
+        override fun getItem(p0: Int): Any {
+            return plants[p0]
+        }
+
+        override fun getItemId(p0: Int): Long {
+            return p0.toLong()
+        }
+
+        override fun getCount(): Int {
+            return plants.size
+        }
     }
 }
